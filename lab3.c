201b31f6c9e1ad57c5d8b8ba436032b17dccd4fe
batya <0867532.org@mail.ru>
2017-03-28 22:45:48 +0300

    Tests passed.

--- lab3.c
+++ lab3.c
@@ -14,32 +14,32 @@
 #define null NULL
 #endif
 
-void MakeArgs(char **Args,char * Line)
+void MakeArgs(char **Args, char *Line)
 {
-	char * Token = strtok(Line, " \n\r");
+	char *Token = strtok(Line, "\n\r ");
 	int Counter = 0;
 
-	while(Token) {
+	while (Token) {
 #ifdef Lab3Debug
-		write(1,"Arg:",sizeof("Arg:"));
-		write(1,Token,strlen(Token));
-		write(1,"\n",sizeof("\n"));
+		write(1, "Arg:", sizeof("Arg:"));
+		write(1, Token, strlen(Token));
+		write(1, "\n", sizeof("\n"));
 #endif
 		Args[Counter++] = Token;
-		Token = strtok(null, " \n\r");
+		Token = strtok(null, "\n\r ");
 	}
 	Args[Counter] = null;
 }
 
-int main()
+int main(void)
 {
 	char InputBuff[Lab3InputBuffLength] = {0};
 	char *Args[Lab3ArgumentsCount] = {0};
 
-	go:
+go:
 
-	if(!getcwd(InputBuff, Lab3InputBuffLength)) {
-		write(1, "Error: Current directory path too long.", 38);
+	if (!getcwd(InputBuff, Lab3InputBuffLength)) {
+		write(1, "Error: Current directory path too long.\n", 38);
 		return Lab3ErrorCWDTooLong;
 	}
 
@@ -50,20 +50,20 @@ int main()
 	read(2, InputBuff, Lab3InputBuffLength);
 	MakeArgs(Args, InputBuff);
 
-	switch(fork()) {
-		case -1:
-			write(1, "Error: Unable to fork process." ,29);
-			return Lab3ErrorForking;
-		case 0:
-			if(strcmp(Args[0], "cd"))
-				execvp(Args[0],Args);
-			write(1, "Warning: Execution failed.\n", 27);
-			break;
-		default:
-			if(!strcmp(Args[0], "cd"))
-				chdir(Args[1]);
-			wait(null);
-			goto go;
+	switch (fork()) {
+	case -1:
+		write(1, "Error: Unable to fork process.\n", 30);
+		return Lab3ErrorForking;
+	case 0:
+		if (strcmp(Args[0], "cd"))
+			execvp(Args[0], Args);
+		write(1, "Warning: Execution failed.\n", 27);
+		break;
+	default:
+		if (!strcmp(Args[0], "cd"))
+			chdir(Args[1]);
+		wait(null);
+		goto go;
 	}
 
 	return 0;
--- makefile
+++ makefile
@@ -2,8 +2,10 @@ default: ./lab3.c
 	gcc -o lab1.run ./lab3.c
 
 test: ./lab3.c
+	reset
 	echo gcc:
 	gcc -o lab1.run ./lab3.c -fsanitize=address
 	echo cppcheck:
 	cppcheck --enable=all --inconclusive --std=posix lab3.c
-	
+	echo codepatch.pl:
+	/home/batya/linux/scripts/checkpatch.pl -f /home/batya/labs/lab3/lab3.c
